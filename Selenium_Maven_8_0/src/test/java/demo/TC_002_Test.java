78723d3b85c46f658e5678a6f2baeb65cbdc24f6
gangu <gangu@Abhishek>
2024-06-13 09:00:14 +0530

    suffix with test

---
+++ Selenium_Maven_8_0/src/test/java/demo/TC_002_Test.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC_002_Test {
+	
+	@Test
+	public void test()
+	{
+		Reporter.log(" TC_002_Test is executed",true);
+	}
+
+}
